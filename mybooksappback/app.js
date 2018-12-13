8e7a5f9c1300764dc46a904da0d6fb0bdbaad340
Omar Avelar <omar.avelar.f@gmail.com>
2018-12-13 13:01:16 -0600

    send form to local database

--- mybooksappback/app.js
+++ mybooksappback/app.js
@@ -53,7 +53,7 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 app.use(cors({
   credentials: true,
-  origin: ['http://localhost/3001']
+  origin: ['http://localhost:3001']
 }))
 
 
--- mybooksappfront/src/components/authors/AddAuthor.js
+++ mybooksappfront/src/components/authors/AddAuthor.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-//import axios from 'axios';
+import axios from 'axios';
 
 class AddAuthor extends Component{
 state={
@@ -8,9 +8,38 @@ state={
   books: []
 }
 
+handleFormSubmit= (event) => {
+    event.preventDefault();
+    const name = this.state.name;
+    const nationallity = this.state.nationallity;
+
+    axios.post('http://localhost:3000/api/authors', {name, nationallity})
+      .then( 
+        this.setState({
+          name: '',
+          nationallity: ''
+        })
+      )
+      .catch(e=> console.log(e))
+}
+
+handleChange = (event) => {
+  const {name, value } = event.target;
+  this.setState({[name]: value});
+
+}
+
+
 render(){
   return(
-    <div>Ya calmate Mike</div>
+    // <div>Ya calmate Mike</div>
+    <div>
+    <form onSubmit={this.handleFormSubmit}>
+    <input name="name" value={this.state.name} type="text" placeholder="Nombre" onChange={this.handleChange} />
+    <input name="nationallity" value={this.state.nationallity} type="text" placeholder="Nacionalidad" onChange={this.handleChange} />
+    <input value="Submit" type="submit" />
+    </form>
+    </div>
   )
 }
 
