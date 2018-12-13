f33b581202385edc933b09d4979b2c380c038934
Omar Avelar <omar.avelar.f@gmail.com>
2018-12-13 11:53:42 -0600

    now frontend

---
+++ mybooksappfront/src/components/authors/AddAuthor.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+//import axios from 'axios';
+
+class AddAuthor extends Component{
+state={
+  name: "",
+  nationallity: '',
+  books: []
+}
+
+render(){
+  return(
+    <div>Ya calmate Mike</div>
+  )
+}
+
+};
+
+
+export default AddAuthor;
---
+++ mybooksappfront/src/components/home/Home.js
@@ -0,0 +1,23 @@
+import React, {Components} from 'react';
+import {Link} from 'react-router-dom';
+
+const Home = () => (
+    <div>   
+        <h1>
+          Mike Ya camate pofavo
+        </h1>
+        <Link>
+        <button>Show me the Mortys</button>
+        </Link>
+
+        <Link>
+        <button>Agrega un Morty</button>
+        </Link>
+
+
+    </div>
+  )
+
+
+
+export default Home;
